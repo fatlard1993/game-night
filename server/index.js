ffa54c964f4878ccb4f8179cd2dbdda427741336
justfatlard <fatlard1993@gmail.com>
2026-07-17 00:54:42 -0700

    swap github argi dep for argi-cli from npm

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 import os from 'os';
 import path from 'path';
 
-import Argi from 'argi';
+import Argi from 'argi-cli';
 
 import database from './database';
 import server, { spawnBuild } from './server';
